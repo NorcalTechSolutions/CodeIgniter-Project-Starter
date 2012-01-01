63fed3176d315130d91e6aa8e11715aaa99d95ce
Joshua Riddle <norcaltechsolutions@gmail.com>
2012-01-01 03:31:40 -0800

    Initial Commit

---
+++ application/logs/log-2012-01-01.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-01-01 02:50:39 --> 404 Page Not Found --> welcome
+ERROR - 2012-01-01 02:50:39 --> ErrorException [ 1 ]: The page you requested was not found. ~  [ 0 ]
